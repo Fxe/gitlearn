c4dbf3055b30dc405167719db0ae090179d3036b
Filipe Liu <filipeliu@hotmail.com>
2012-11-10 12:30:00 +0000

    Initial

---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstdlib>
+
+
+#define DEFAULT_SIZE 100
+#define DEFAULT_VALUE 1
+
+int main( int argc, char **argv) {
+	int size = DEFAULT_SIZE;
+
+
+	double* v1 = new double[size];
+
+
+	for ( int i = 0; i < size; i++)
+		v1[i] = DEFAULT_VALUE;
+
+
+	double sum = 0;
+
+	for ( int i = 0; i < size; i++)
+		sum += v1[i];
+
+
+	std::cout << "SUM:" << sum << std::endl;
+
+	return 0;
+}
